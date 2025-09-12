342c3a6aec43666a621953d4151badc7df9a5851
xiaolei20050102 <1227233285@qq.com>
2025-09-12 18:37:44 +0800

    commit A.cpp

---
+++ A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+const int maxn = 2e6 + 7;
+void solve() {
+    cout << "kkk" << endl;
+}
+void o2() {
+    ios::sync_with_stdio(false), cin.tie(nullptr),cout.tie(nullptr);
+}
+void test() {
+    o2();
+    int t = 1;
+    cin >> t;
+    while (t--)solve();
+}
+signed main() {
+    test();
+    return 0;
+}
